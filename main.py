c02222a5cddc560eb66303c60958a322d37ad0cf
Brette Fitzgibbon <79230918+BretteConnolly@users.noreply.github.com>
2022-07-30 01:25:13 +0000

    Added comments

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput("Make Your Bet", "Which turtle will win the race? Pick a color: ")
+user_bet = screen.textinput("Make Your Bet", "Which turtle will win the race? Pick a color: ") #creates a graphical user interface in which to type one's bet
 
 colors = ["yellow", "orange", "red", "purple", "green", "blue"]
-y_coordinates = [165, 99, 33, -33, -99, -165]
-all_turtles = []
+y_coordinates = [165, 99, 33, -33, -99, -165] #turtles placed equidistant from one another in a vertical line
+all_turtles = [] #turtles in a list to iterate on
 is_race_on = False
 
 for turtle in range(0, 6):
-  new_turtle = Turtle("turtle")
+  new_turtle = Turtle("turtle") #create 6 turtles
   new_turtle.color(colors[turtle])
   new_turtle.penup()
-  new_turtle.goto(-230, y_coordinates[turtle])
+  new_turtle.goto(-230, y_coordinates[turtle]) #starting line
   all_turtles.append(new_turtle)
 
 if user_bet:
@@ -22,7 +22,7 @@ if user_bet:
   
 while is_race_on:
   for turtle in all_turtles:
-    if turtle.xcor() > 230:
+    if turtle.xcor() > 230: #finish line
       is_race_on = False
       print(f"The {turtle.pencolor()} turtle won!")
       if user_bet == turtle.pencolor():
@@ -31,7 +31,7 @@ while is_race_on:
         print("You lost.")
     else:
       rand_distance = randint(0, 10)
-      turtle.forward(rand_distance)
+      turtle.forward(rand_distance) #every turtle moves forward a different randomly generated distance 
     
 
 
